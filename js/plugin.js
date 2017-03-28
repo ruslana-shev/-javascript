1989afb39fdda8e9188b2dfacc82b0aa831f2b67
ruslana-shev <ruslana.shevandina@gmail.com>
2017-03-28 19:28:58 +0300

    Решение задач урок 23

---
+++ js/plugin.js
@@ -0,0 +1,75 @@
+///////////////Часть1//////////////////////////////
+
+//var str = 'I am in the easycode';
+//var arr = str.split(' ');
+//for (var i = 0, max = arr.length; i < max; i++) {
+//	arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+//	console.log(arr.join(' '));
+//}
+
+
+//var str = "tseb eht ma i";
+//var arr = str.split('');
+//for (var i = 0; i < arr.length; i++) {
+//	arr.reverse(); 
+//	console.log(arr.join(' ')); break;
+//}
+
+
+//var i = 10;
+//var factorial = 1;
+//while (i--) {
+//	console.log(factorial *= i+1);
+//}
+
+
+//var str = 'Считаем до 10и: ';
+//for (var i = 1; i <= 10; i++) {
+//	str += ( i==10 ) ? i : i + ', ';
+//	console.log(str);
+//}
+
+
+//var str = 'JavaScript is a pretty good language';
+//var arr = str.split(' ');
+//for (var i = 0, max = arr.length; i < max; i++) {
+//	arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+//	console.log(arr.join(''));
+//}
+
+
+//let i = 0;
+//for ( ; i <= 15; i++) {
+//	if(i % 2 == 0){
+//		continue;
+//	}
+//	console.log(i);
+//}
+
+/////////////////Часть2////////////////////////////////
+//4
+//var arr = ['one', 2, 'three', 4];
+//arr.splice(4, 0, 'one', 2, 'three', 4)
+
+//5
+//var arr = [1,2,3,4,5];
+// arr.splice(2,2);
+//console.log(arr);
+
+//6
+//arr. splice(2,2, 'three', 'four');
+//console.log(arr);
+
+//7
+//var arr = ['I', 'am', 'an', 'array'];
+//arr.splice(3,0, 'awesome');
+//console.log(arr);
+
+//8
+//var arr = [[14,45], [1], ['a','c','d']];
+
+//9
+//var arr = [1, 5];
+//var newArr = arr.concat(3, 4);
+//var newArr = arr.slce();
+//console.log(newArr);
